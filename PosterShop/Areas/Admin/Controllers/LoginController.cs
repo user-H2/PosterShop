32efd178c4edc5fb98e32862cb23b463af9d5c21
DESKTOP-C1BV5EG\Admin <2002nguyenvanhuynh@gmail.com>
2022-11-22 01:20:36 +0700

    Login with Membership Provider

---
+++ PosterShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using PosterShop.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace PosterShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(LoginModel model)
+        {
+            if (Membership.ValidateUser(model.UserName,model.Password) && ModelState.IsValid)
+            {
+                FormsAuthentication.SetAuthCookie(model.UserName,model.RememberMe);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("","Tên đăng nhập hoặc mật khẩu không hợp lệ!");
+            }
+            return View(model);
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ PosterShop/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PosterShop.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
