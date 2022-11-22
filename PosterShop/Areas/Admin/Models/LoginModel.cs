8cec7462e5f3e23fe0a89463bde639fa30875ab6
DESKTOP-C1BV5EG\Admin <2002nguyenvanhuynh@gmail.com>
2022-11-22 23:27:52 +0700

    fix some bug of login

--- PosterShop/Areas/Admin/Models/LoginModel.cs
+++ PosterShop/Areas/Admin/Models/LoginModel.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace PosterShop.Areas.Admin.Models
 {
     public class LoginModel
     {
-        [Required]
+        [Required(ErrorMessage = "Mời nhập user name")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Mời nhập password")]
         public string Password { get; set; }
+
         public bool RememberMe { get; set; }
     }
 }
